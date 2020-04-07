28a5b68deae4fa951d3a09dd74ea837aed9724b5
Tom Flídr <tomflidr@gmail.com>
2020-04-07 19:19:54 +0200

    release 3.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Example - Hello World - Javascript
 
-[![Latest Stable Version](https://img.shields.io/badge/Stable-v2.1.0-brightgreen.svg?style=plastic)](https://github.com/web-dev-server/example-helloworld/releases)
+[![Latest Stable Version](https://img.shields.io/badge/Stable-v3.0.0-brightgreen.svg?style=plastic)](https://github.com/web-dev-server/example-helloworld/releases)
 [![License](https://img.shields.io/badge/Licence-BSD-brightgreen.svg?style=plastic)](https://github.com/web-dev-server/example-helloworld/blob/master/LICENCE.md)
 
 Hallo world example with static and dynamic content in Javascript.
--- run-server.js
+++ run-server.js
@@ -1,10 +1,11 @@
-var WebDevServer = require("web-dev-server");
+//var WebDevServer = require("web-dev-server");
+var WebDevServer = require("../web-dev-server/build/lib/Server");
 
 
 // Create logger for any uncatched error (optional):
-var logger = WebDevServer.Logger.CreateNew(__dirname, __dirname)
+var logger = WebDevServer.Tools.Logger.CreateNew(__dirname, __dirname)
 	.SetStreamWriting(true)
-	.SetAllowedLevels([WebDevServer.Logger.LEVEL.ERROR, WebDevServer.Logger.LEVEL.CRITICIAL])
+	.SetAllowedLevels([WebDevServer.Tools.Logger.LEVEL.ERROR, WebDevServer.Tools.Logger.LEVEL.CRITICIAL])
 	.SetMaxLogFileSize('100M')
 	.SetStackTraceWriting(true, true);
 
@@ -16,34 +17,29 @@ WebDevServer.Server.CreateNew()
 	// Optional, 8000 by default.
 	.SetPort(8000)
 	// Optional, '127.0.0.1' by default.
-	//.SetDomain('127.0.0.1')
+	.SetHostname('127.0.0.1')
 	// Optional, `true` by default to display Errors and directories.
 	//.SetDevelopment(false)
-	// Optional, 1 hour by default (seconds).
-	//.SetSessionMaxAge(60 * 60 * 24)
-	// Optional, session id hash salt.
-	//.SetSessionHash('SGS2e+9x5$as%SD_AS6s.aHS96s')
 	// Optional, `null` by default, useful for apache proxy modes.
 	//.SetBaseUrl('/node')
 	// Optional, custom place to log any unhandled errors.
-	.SetErrorHandler(function (err, code, req, res) {
+	.SetErrorHandler(async function (err, code, req, res) {
+		console.error(err);
 		logger.Error(err);
 	})
 	// Optional, to prepend any execution before `web-dev-server` module execution.
-	.AddHandler(function (req, res, evnt, cb) {
-		if (req.url == '/health') {
-			res.writeHead(200);
-			res.end('1');
+	.AddPreHandler(async function (req, res, event) {
+		if (req.GetPath() == '/health') {
+			res.SetCode(200).SetBody('1').Send();
 			// Do not anything else in `web-dev-server` module for this request:
-			evnt.PreventDefault();
+			event.PreventDefault();
 		}
 		/*setTimeout(() => {
 			throw new RangeError("Uncatched test error.");
 		}, 1000);*/
-		cb();
 	})
 	// Callback param is optional. called after server has been started or after error ocured.
-	.Run(function (success, err) {
-		if (!success) console.error(err);
+	.Start(function (success, err) {
+		if (!success) return console.error(err);
 		console.log("Server is running.");
 	});
